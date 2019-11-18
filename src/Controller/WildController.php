792ce2c264425b4d7bb61bcbfe4891f85f93b13a
Céline Godichon <celine.godichon@gmail.com>
2019-11-18 10:56:37 +0100

    Quest #5 Symfony. Routing & annotations

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,15 +7,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
+
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="index")
      */
     public function index() :Response
     {
-        return $this->render('index.html.twig', [
+        return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{page<^[a-z0-9]+(?:-[a-z0-9]+)*$>}", name="show")
+     * @param string $page
+     * @return Response
+     */
+    public function show(string $page = " "): Response
+    {
+        if($page === " ") {
+             $page = "Aucune série sélectionnée, veuillez choisir une série";
+        }else{
+            $page = ucwords(str_replace('-', ' ', $page));
+
+        }return $this->render('wild/show.html.twig', ['page' => $page]);
     }
 }
